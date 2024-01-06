6254cbe2b13f58186a48843edb2d4a7cbcb021dc
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-06 16:11:45 +0200

    next app config

---
+++ next-app/app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import AddToCart from '../AddToCart';
+
+
+
+
+const ProductCard = () => {
+  return (
+      <div className="p-2 my-5 bg-slate-300 hover:bg-slate-600"><AddToCart/></div>
+  )
+}
+
+export default ProductCard
